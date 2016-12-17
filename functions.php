b6f44d4942eb9a04d5f007840326ab2e7c6a00fd
unknown <benursal@hotmail.com>
2016-12-18 07:02:19 +0800

    css

--- functions.php
+++ functions.php
@@ -13,3 +13,24 @@ add_theme_support( 'genesis-responsive-viewport' );
 
 //* Remove the entry title (requires HTML5 theme support)
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
+
+
+// SHORTCODES 
+
+function search_medicine_func( $atts ){
+	
+	$html = '<div class="search-box">
+				<h3>Find Your Specialty Drug</h3>
+				<form>
+					<input type="text" class="form-control input-lg" placeholder="enter brand or generic name" /> 
+					<button type="submit" class="btn btn-cta">Search Drugs</button>
+				</form>
+				<p>
+					or <a href="#">Browse all Drugs</a>
+				</p>
+			 </div>';
+	
+	return $html;
+}
+add_shortcode( 'search_medicine', 'search_medicine_func' );
