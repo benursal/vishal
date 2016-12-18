b420304536022862ea2bf9747d7f507403501335
unknown <benursal@hotmail.com>
2016-12-18 09:08:43 +0800

    making progress

--- functions.php
+++ functions.php
@@ -14,7 +14,13 @@ add_theme_support( 'genesis-responsive-viewport' );
 //* Remove the entry title (requires HTML5 theme support)
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 
+// Load Font Awesome
+add_action( 'wp_enqueue_scripts', 'enqueue_font_awesome' );
+function enqueue_font_awesome() {
 
+	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' );
+
+}
 
 // SHORTCODES 
 
@@ -40,3 +46,32 @@ function search_medicine_func( $atts ){
 	return $html;
 }
 add_shortcode( 'search_medicine', 'search_medicine_func' );
+
+function featured_disease_categories_func( $atts ){
+	
+	$html = '<div class="d-categories">
+				<div class="my-row">
+					<div class="column-left left">
+						<a href="#" class="btn btn-md btn-block btn-simple">
+							Breast Cancer <i class="fa fa-caret-right" aria-hidden="true"></i>
+						</a>
+					</div>
+					<div class="column-left right">
+						<a href="#" class="btn btn-md btn-block btn-simple">
+							Breast Cancer <i class="fa fa-caret-right" aria-hidden="true"></i>
+						</a>
+					</div>
+					<div style="clear:both"></div>
+				</div>
+			 </div>
+			 
+			 <p class="text-center">
+				<a href="#" class="btn btn-browse-all">
+					Browse All Diseases <i class="fa fa-caret-right" aria-hidden="true"></i>
+				</a>
+  			 </p>';
+			 
+	return $html;
+}
+
+add_shortcode( 'featured_disease_categories', 'featured_disease_categories_func' );
