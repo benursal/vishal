798942e64b016cce7a0be7f2db319cb9c477041f
unknown <benursal@hotmail.com>
2017-01-10 06:03:42 +0800

    media queries

--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ add_action( 'wp_enqueue_scripts', 'enqueue_font_awesome' );
 function enqueue_font_awesome() {
 
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' );
+	wp_enqueue_style( 'media-queries', get_stylesheet_directory_uri() . '/media-queries.css?ver='.date('YmdHis') );
 
 }
 
@@ -90,3 +91,74 @@ function featured_disease_categories_func( $atts ){
 }
 
 add_shortcode( 'featured_disease_categories', 'featured_disease_categories_func' );
+
+
+
+
+/**
+ * Add the mobile responsive menu - "footer nav with anchor" 
+ *
+ * @author John Sundberg
+ * @link http://blackhillswebworks.com/?p=4852
+ */
+
+	// Register "Responsive Footer Menu" widget area 
+	add_action( 'widgets_init', 'bhww_register_responsive_footer_menu_widget_area' );
+ 
+	function bhww_register_responsive_footer_menu_widget_area() {
+ 
+		// Added this conditional for the WordPress 3.7 update
+		if ( function_exists( 'genesis_register_sidebar' ) ) {
+		
+			genesis_register_sidebar( array(
+				'id'            => 'footer-nav-menu',
+				'name'          => __( 'Responsive Footer Menu', '' ),
+				'description'   => __( 'This widget area is displayed above the footer widgets on mobile devices (600px wide or less), and should be used for one or more Custom Menu widgets.', '' ),
+			) );
+			
+		}
+		
+	}
+	
+ 
+	// Check to see if there are any widgets in the Responsive Footer Menu widget area
+	if ( is_active_sidebar( 'footer-nav-menu' ) ) {
+	
+		// Add the header link and footer menu HTML containers
+		add_action( 'genesis_before', 'bhww_footer_anchor_responsive_menu' );
+	 
+		function bhww_footer_anchor_responsive_menu() {
+			
+			// Add the header navicon container, BEFORE the #title-area (.title-area in HTML5)
+			add_action( 'genesis_header', 'bhww_footer_anchor_header_link', 6 );
+			
+			function bhww_footer_anchor_header_link() {
+			
+				echo '<div class="mobile-footer-nav-link"><a href="#mobile-footer-nav-menu" class="btn button mobile-nav-link-button"><span class="mobile-footer-nav-link-text">Menu</span> <span class="mobile-footer-nav-link-icon">&#9660;</span></a></div>';
+				
+			}
+			
+			
+			// Add the footer menu container, BEFORE the footer
+			add_action( 'genesis_before_footer', 'bhww_footer_anchor_menu_container', 5 );
+			
+			function bhww_footer_anchor_menu_container() {
+			
+				echo '<div id="mobile-footer-nav-menu"><div class="wrap">';
+				
+				echo '<div class="mobile-footer-nav-menu-top-link"><a href="#" class="btn button">Back to Top</a></div>';
+				
+				genesis_widget_area( 'footer-nav-menu', array(
+			
+					'before'=>	'<div class="mobile-footer-nav-menu-widgets widget-area">', 
+					'after'	=>	'<div class="clear"></div></div><!-- end .mobile-footer-nav-menu-widgets -->'
+			
+				) );
+				
+				echo '</div></div>';
+				
+			}
+
+		}
+		
+	}
